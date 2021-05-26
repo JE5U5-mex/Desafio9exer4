292d78267850883c03726916456570dd47fae21e
JE5U5-max <andreynogueira904@gmail.com>
2021-05-26 16:30:47 -0300

    Desafio9 Exercício4 26/05/2021

---
+++ src/desafio9/Desafio9.java
@@ -0,0 +1,21 @@
+/*
+screver um algoritmo que gere e escreva os números ímpares entre 100 e 200.
+*/
+package desafio9;
+
+/**
+ *
+ * @author andrey josè nogueira
+ */
+public class Desafio9 {
+
+    public static void main(String[] args) {
+    int numeros;
+    numeros=101;
+    do{
+        System.out.println("numeros ímpares "+numeros);
+        numeros=numeros+2;
+    }while(numeros<200);
+    }
+    
+}
